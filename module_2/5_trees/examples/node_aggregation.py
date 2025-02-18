eedfbae0dc2aa317551dfa9c44c872c7e2b6dae8
Yura <yukhldkv@gmail.com>
2025-02-18 10:23:55 -0800

    more tree aggregation examples

--- module_2/5_trees/examples/node_aggregation.py
+++ module_2/5_trees/examples/node_aggregation.py
@@ -29,3 +29,82 @@ def get_nodes_count(node):
 
 get_nodes_count(tree)
 # 8
+
+
+from hexlet import fs
+
+tree = fs.mkdir('/', [
+    fs.mkdir('etc', [
+        fs.mkdir('apache'),
+        fs.mkdir('nginx', [
+            fs.mkfile('nginx.conf'),
+        ]),
+    ]),
+    fs.mkdir('consul', [
+        fs.mkfile('config.json'),
+        fs.mkfile('file.tmp'),
+        fs.mkdir('data'),
+    ]),
+    fs.mkfile('hosts'),
+    fs.mkfile('resolve'),
+])
+
+
+def get_files_count(node):
+    if fs.is_file(node):
+        return 1
+    children = fs.get_children(node)
+    descendant_count = list(map(get_files_count, children))
+    return sum(descendant_count)
+
+
+def get_subdirectories_info(node):
+    children = fs.get_children(node)
+    # Нас интересуют только директории
+    filtered = filter(fs.is_directory, children)
+    # Запускаем подсчет для каждой директории
+    return list(map(lambda child: (fs.get_name(child), get_files_count(child)), filtered))
+
+
+print(get_subdirectories_info(tree))
+# => [('etc', 1), ('consul', 2)]
+
+
+items = [2, [3, [4, [5, [6]]], [7]]]
+
+def aggregate(items):
+    if isinstance(items, int):
+        return items
+    result = list(map(aggregate, items))
+    return sum(result)
+
+print(aggregate(items))
+
+
+items = [2, [3, [4, [5, [6]]], [7]]]
+
+def check(num):
+    if num % 2 == 0 and num > 4:
+        return num
+    return 1
+
+def aggregate(items):
+    if isinstance(items, int):
+        return check(items)
+    result = list(map(aggregate, items))
+    return sum(result)
+
+aggregate(items) # 11
+
+
+# Функция count_items() считает суммарное количество элементов в списке и во вложенных списках.
+
+items = [2, ['a', [4, ['s', [6, [1.5]]]]]]
+
+def count_items(items):
+    if not isinstance(items, list):
+        return 1
+    result = sum(list(map(count_items, items)))
+    return result
+
+count_items(items) # 6
